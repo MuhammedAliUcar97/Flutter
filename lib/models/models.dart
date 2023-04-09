6b9faca3a529c6c11b54a299c45b550e17f154ac
mamimo2353 <115897973+mamimo2353@users.noreply.github.com>
2023-04-10 01:15:53 +0300

    Add files via upload

---
+++ lib/models/models.dart
@@ -0,0 +1,7 @@
+class Models {
+  int id;
+  String baslik;
+  int derskredisi;
+  int dersnotu; 
+  Models({required this.id, required this.baslik, required this.derskredisi, required this.dersnotu});
+}
