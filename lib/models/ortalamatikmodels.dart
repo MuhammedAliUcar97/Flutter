4d2acf0439fb6a0c0466ec7951f841d1cfd30f83
mamimo2353 <115897973+mamimo2353@users.noreply.github.com>
2023-04-12 11:37:08 +0300

    Delete lib directory

--- lib/models/ortalamatikmodels.dart
+++
@@ -1,7 +0,0 @@
-class Models {
-  int id;
-  String baslik;
-  int derskredisi;
-  int dersnotu; 
-  Models({required this.id, required this.baslik, required this.derskredisi, required this.dersnotu});
-}
